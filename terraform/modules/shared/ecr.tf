14611e6e601c7b4e5564a7d0e78011549e1579d9
Vinicius Marinho <viniciusmarinho@MacBook-Pro-de-Vinicius.local>
2025-11-19 13:23:52 -0300

    fix

--- terraform/modules/shared/ecr.tf
+++ terraform/modules/shared/ecr.tf
@@ -9,3 +9,34 @@ resource "aws_ecr_repository" "ecr" {
     Name = "eloquent/${var.repository_name}"
   }
 }
+
+
+
+data "aws_iam_policy_document" "ecr_ecs_policy_document" {
+  statement {
+    sid    = "ECSAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImageScanFindings",
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "ecr_write_access_policy" {
+  repository = aws_ecr_repository.ecr.name
+  policy     = data.aws_iam_policy_document.ecr_ecs_policy_document.json
+}
